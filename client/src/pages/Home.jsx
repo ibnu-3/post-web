c13fc0a1631ee79356ba1e9ab4c8553c3eec0dd5
ibnu-3 <ibnuturab3@gmail.com>
2025-09-02 03:05:00 +0000

    completed

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,9 +2,9 @@ import React, { useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { logout } from '../slices/authSlice';
-import { getPosts } from '../slices/postSlice';
+import {  getPosts } from '../slices/postSlice';
 import { toast } from 'react-toastify';
-import { MdDelete, MdModeEdit, MdRemoveRedEye } from "react-icons/md";
+import {  MdModeEdit, MdRemoveRedEye } from "react-icons/md";
 const Home = () => {
     const navigate = useNavigate();
     const dispatch = useDispatch();
@@ -30,6 +30,17 @@ const Home = () => {
                 toast.error('Logout failed');
             });
     };
+    //  const handleDelete = (id) => {
+               
+    //             dispatch(deletePost({ postId: id}))
+    //                 .then(() => {
+    //                     toast.success('Post deleted successfully!');
+                        
+    //                 })
+    //                 .catch(() => {
+    //                     toast.error(isError ? message : 'Failed to delete post');
+    //                 });
+    //         };
 
     if (isLoading) {
         return <div>Loading...</div>;
@@ -59,6 +70,7 @@ const Home = () => {
                             <div className='flex justify-start items-center '>
                                 <Link to={`/edit-post/${post._id}`} className='rounded text-blue-500 px-4 '><MdModeEdit/></Link>
                                 <Link to={`/view-post/${post._id}`} className='rounded text-cyan-500 px-4 '><MdRemoveRedEye /></Link>
+                                {/* <button className='text-red-600' ><MdDelete/></button> */}
                             </div>
                         </div>
                     ))}
@@ -68,7 +80,7 @@ const Home = () => {
                 <p>No posts found.</p>
                 <Link to={'/create-post'} className='px-5 py-2.5 bg-green-500 text-white rounded'>Create Post</Link></>
             )}
-            <button className='px-5 py-2.5 bg-green-500 text-white rounded-full  font-extrabold text-4xl bottom-4 right-4 fixed' onClick={()=>navigate('/create-post')}>+</button>
+            <button  className='px-5 py-2.5 bg-green-500 text-white rounded-full  font-extrabold text-4xl bottom-4 right-4 fixed' onClick={()=>navigate('/create-post')}>+</button>
         </div>
     );
 };
